2230a6750b549956dd13c9076ce987d6896de482
Steffen Zeidler <sigma_z@sigma-scripts.de>
2014-01-16 09:29:02 +0100

    Added DriverFactory

---
+++ lib/Dive/Connection/Driver/DriverFactory.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * This file is part of the Dive ORM framework.
+ * (c) Steffen Zeidler <sigma_z@sigma-scripts.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Dive\Connection\Driver;
+
+/**
+ * @author  Steffen Zeidler <sigma_z@sigma-scripts.de>
+ * @created 16.01.14
+ */
+class DriverFactory
+{
+
+    /**
+     * @param  string $scheme
+     * @throws DriverException
+     * @return DriverInterface
+     */
+    public static function createBySchema($scheme)
+    {
+        $className = '\\Dive\\Connection\Driver\\' . ucfirst($scheme) . 'Driver';
+        if (class_exists($className)) {
+            return new $className;
+        }
+        throw new DriverException("Could not load driver for scheme '$scheme'!");
+    }
+
+
+    /**
+     * @param  string $dsn
+     * @return DriverInterface
+     * @throws DriverException
+     */
+    public static function createByDsn($dsn)
+    {
+        $length = strpos($dsn, ':');
+        if ($length === false) {
+            throw new DriverException("Could not load driver for data source name (dsn) '$dsn'!");
+        }
+        $scheme = substr($dsn, 0, $length);
+        return self::createBySchema($scheme);
+    }
+
+}

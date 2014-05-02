fc56a0266c77f55b6796b2fd45c39bffccdc8113
Steffen Zeidler <sigma_z@sigma-scripts.de>
2014-05-02 17:01:58 +0200

    Moved class comments to their places

--- lib/Dive/Hydrator/SingleScalarHydrator.php
+++ lib/Dive/Hydrator/SingleScalarHydrator.php
@@ -6,16 +6,15 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-/**
- * @author  Steffen Zeidler <sigma_z@sigma-scripts.de>
- * @created 19.03.13
- */
 
 namespace Dive\Hydrator;
 
 use Dive\Table;
 
-
+/**
+ * @author  Steffen Zeidler <sigma_z@sigma-scripts.de>
+ * @created 19.03.13
+ */
 class SingleScalarHydrator extends SingleHydrator
 {
 

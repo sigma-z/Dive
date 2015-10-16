234a3dd637548d8b6e7de73e48cce97a510460de
Mike Gladysch <mail@mike-gladysch.de>
2015-10-16 16:10:35 +0200

    merged db configurations for travis from master too

--- phpunit_db_config.travis.php
+++ phpunit_db_config.travis.php
@@ -1,21 +1,5 @@
 <?php
-/*
- * This file is part of the Dive ORM framework.
- * (c) Steffen Zeidler <sigma_z@sigma-scripts.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-/**
- * @author Steffen Zeidler <sigma_z@sigma-scripts.de>
- * Date: 15.11.12
- */
-
-return array(
-    'sqlite:' . FIXTURE_DIR . '/test.db',
-    array(
-        'dsn' => 'mysql:host=127.0.0.1;dbname=dive',
-        'user' => 'root',
-        'password' => ''
-    )
+return array_merge(
+    include 'phpunit_db_config.travis.sqlite.php',
+    include 'phpunit_db_config.travis.mysql.php'
 );

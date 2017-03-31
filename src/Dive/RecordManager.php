6e7b4b259b5a41dc55272b4eeb11130e357a6f8c
Steffen Zeidler <sigma_z@sigma-scripts.de>
2017-03-31 18:08:42 +0200

    Committing version number 1.0.8

--- src/Dive/RecordManager.php
+++ src/Dive/RecordManager.php
@@ -25,7 +25,7 @@ use Dive\Validation\ValidationContainer;
 class RecordManager
 {
 
-    const ORM_VERSION = '1.0.7';
+    const ORM_VERSION = '1.0.8';
 
     const FETCH_RECORD_COLLECTION = 'record-collection';
     const FETCH_RECORD = 'record';

af1ff04d38b10469e92c81ef1a4977ca3d42eecd
Steffen Zeidler <sigma_z@sigma-scripts.de>
2017-10-27 14:06:39 +0200

    versioned to 1.0.13

--- src/Dive/RecordManager.php
+++ src/Dive/RecordManager.php
@@ -25,7 +25,7 @@ use Dive\Validation\ValidationContainer;
 class RecordManager
 {
 
-    const ORM_VERSION = '1.0.12';
+    const ORM_VERSION = '1.0.13';
 
     const FETCH_RECORD_COLLECTION = 'record-collection';
     const FETCH_RECORD = 'record';

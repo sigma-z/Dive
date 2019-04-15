24e330ed6962e15722c82c003aed0669c332928d
Mike Gladysch <mail@mike-gladysch.de>
2019-04-15 07:47:50 +0200

    versioned to 1.0.15

--- src/Dive/RecordManager.php
+++ src/Dive/RecordManager.php
@@ -25,7 +25,7 @@ use Dive\Validation\ValidationContainer;
 class RecordManager
 {
 
-    const ORM_VERSION = '1.0.13';
+    const ORM_VERSION = '1.0.15';
 
     const FETCH_RECORD_COLLECTION = 'record-collection';
     const FETCH_RECORD = 'record';
